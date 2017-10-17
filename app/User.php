6d9dc3b358708a80166242e01d61387f52a73474
Glendon Small <gsmall@sonovate.com>
2017-10-17 22:11:03 +0100

    Added database file

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     
-    protected $table = 'users_users';
+    protected $table = 'users';
     
     protected $fillable = [
         'name', 'email', 'password', 'firstName', 'lastName','permissions','activated',
